c855ba126dae13ec0e8428d79fc0e14f5834396f
kyongha <magpia001@gmail.com>
2022-12-26 03:18:18 +0900

    category, tag 필터, 좋아요, comment vue, axios 구현

--- api/utils.py
+++ api/utils.py
@@ -1,12 +1,10 @@
 def obj_to_post(obj, flag=True):
     """
-    obj의 각 속성을 serialize 해서, dict로 변환함
-    serialize: python object ->> (기본 타입) int, float, str 으로 형변환 하는 과정
+    obj 의 각 속성을 serialize 해서, dict 로 변환한다.
+    serialize: python object --> (기본 타입) int, float, str
     :param obj:
-    :param flag: True(모두 보냄, /api/post/99/ 용), False(일부 보냄, /api/post/list/ 용)
+    :param flag: True (모두 보냄, /api/post/99/ 용), False (일부 보냄, /api/post/list/ 용)
     :return:
-
-    vars() : 데이터를 dict type으로 변형하는 python 내장 함수
     """
     post = dict(vars(obj))
 
@@ -14,29 +12,25 @@ def obj_to_post(obj, flag=True):
         post['category'] = obj.category.name
     else:
         post['category'] = 'NoCategory'
-    
+
     if obj.tags:
         post['tags'] = [t.name for t in obj.tags.all()]
     else:
         post['tags'] = []
-    
+
     if obj.image:
         post['image'] = obj.image.url
     else:
-        post['image'] = f'https://picsum.photos/id/{obj.id}/900/400'
+        post['image'] = 'https://via.placeholder.com/900x300/'
 
     if obj.update_dt:
         post['update_dt'] = obj.update_dt.strftime('%Y-%m-%d %H:%M:%S')
     else:
         post['update_dt'] = '9999-12-31 00:00:00'
-    
-    # django manage.py shell에서 값 vars(Post.objects.all()[0]) 결과 확인하기
-    # vars() 함수 결과에는 manytomay 필드 결과는 없음
-    del post['_state'], post['category_id'], post['create_dt']
 
-    # /api/post/list/ 일경우, 삭제
-    if not flag:   
-            del post['tags'], post['update_dt'], post['description'], post['content']
+    del post['_state'], post['category_id'], post['create_dt']
+    if not flag:
+        del post['tags'], post['update_dt'], post['description'], post['content']
 
     return post
 
@@ -45,21 +39,33 @@ def prev_next_post(obj):
     try:
         prevObj = obj.get_previous_by_update_dt()
         prevDict = {
-            'id': prevObj.id, 
+            'id': prevObj.id,
             'title': prevObj.title,
         }
     except obj.DoesNotExist:
         prevDict = {}
-    
+
     try:
         nextObj = obj.get_next_by_update_dt()
         nextDict = {
-            'id': nextObj.id, 
+            'id': nextObj.id,
             'title': nextObj.title,
         }
     except obj.DoesNotExist:
         nextDict = {}
 
-    return prevDict, nextDict    
+    return prevDict, nextDict
 
-    
+
+def obj_to_comment(obj):
+    """ comment 객체를 serialize 한다. """
+    comment = dict(vars(obj))
+
+    if obj.update_dt:
+        comment['update_dt'] = obj.update_dt.strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        comment['update_dt'] = '9999-12-31 00:00:00'
+
+    del comment['_state'], comment['post_id'], comment['create_dt']
+
+    return comment
